a10847ec4df46d0eef27d7d38c411d3e14585fd1
Anthony Perugini <Anthony.r.perugini@gmail.com>
2021-06-09 00:51:18 -0400

    numpy everywhere + memoization

--- donut.py
+++ donut.py
@@ -1,8 +1,8 @@
 #! /home/spicy/miniconda3/envs/donut/bin/python
-
 import numpy as np
+from collections import defaultdict
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 50, 50
+SCREEN_WIDTH, SCREEN_HEIGHT = 48, 48
 A_SPEED = 0.07
 B_SPEED = 0.02
 
@@ -10,12 +10,17 @@ R1, R2 = 1, 2
 K2 = 5
 K1 = SCREEN_WIDTH*K2*3/(8*(R1+R2))
 
-THETA_SPACING = 100
-PHI_SPACING = 100
-TWO_PI = 2 * np.pi
+THETA_SPACING = 150
+PHI_SPACING = 150
+TWO_PI = round(2 * np.pi, 2)
 
+theta_sin_memo = defaultdict(int)
+theta_cos_memo = defaultdict(int)
+phi_sin_memo = defaultdict(int)
+phi_cos_memo = defaultdict(int)
 
 def main():
+
     A = B = 1
     while 1:
         sinA, cosA = np.sin(A), np.cos(A)
@@ -26,16 +31,29 @@ def main():
 
 
 def render_torus(sinA, cosA, sinB, cosB):
-    zbuff = [[0 for _ in range(SCREEN_WIDTH)] for _ in range(SCREEN_HEIGHT)]
-    output = [[" " for _ in range(SCREEN_WIDTH)] for _ in range(SCREEN_HEIGHT)]
+    zbuff = np.zeros((SCREEN_WIDTH, SCREEN_HEIGHT))
+    output = np.full((SCREEN_WIDTH, SCREEN_HEIGHT), ' ')
 
     for theta in np.linspace(0, TWO_PI, THETA_SPACING): # np.arange(0, 6.28, THETASPACING):
-        sintheta, costheta = np.sin(theta), np.cos(theta)
-        x_org = R2 + (R1*costheta)
-        y_org = R1*sintheta
+
+        if not theta_sin_memo[theta]:
+            theta_sin_memo[theta] = np.sin(theta)
+        if not theta_cos_memo[theta]:
+            theta_cos_memo[theta] = np.cos(theta)
+            
+        sintheta, costheta = theta_sin_memo[theta], theta_cos_memo[theta]
+
+        x_org = R2 + (R1 * costheta)
+        y_org = R1 * sintheta
         
         for phi in np.linspace(0, TWO_PI, PHI_SPACING): # np.arange(0, 6.28, PHI_SPACING):
-            sinphi, cosphi = np.sin(phi), np.cos(phi)
+
+            if not phi_sin_memo[theta]:
+                phi_sin_memo[phi] = np.sin(phi)
+            if not phi_cos_memo[theta]:
+                phi_cos_memo[phi] = np.cos(phi)
+
+            sinphi, cosphi = phi_sin_memo[phi], phi_cos_memo[phi]
 
             # rot_Y = np.array([[cosphi, 0, sinphi], [0, 1, 0], [-sinphi, 0, cosphi]])
             # rot_A = np.array([[1, 0, 0], [0, cosA, sinA], [0, -sinA, cosA]])
@@ -64,15 +82,14 @@ def render_torus(sinA, cosA, sinB, cosB):
             if (L > 0):
                 if (ooz > zbuff[yp][xp]):
                     zbuff[yp][xp] = ooz
-                    index = int(L*8)
+                    index = int(L * 8)
                     output[yp][xp] = ".,-~:;=!*#$@"[index]
-
 
     for col in output:
         for value in col:
             print(value, end=" ")
         print()
-
+ 
 
 if __name__ == '__main__':
     main()
